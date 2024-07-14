61bd9aa0ca04761e03865ac028d840120d2bbfe7
leehyeonmin <leehyeonmin34@gmail.com>
2024-07-14 16:25:01 +0900

    chore : 초기 셋팅

---
+++ module-project-1/src/test/java/com/leehm34/module_project_1/ModuleProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leehm34.module_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
